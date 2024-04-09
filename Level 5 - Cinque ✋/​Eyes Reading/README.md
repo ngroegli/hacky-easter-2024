ef2f9d3c3a50e5c8c967928ef17825cabdd32cfd
Nicola Groegli <nicola@groeg.li>
2024-04-09 10:50:39 +0200

    solved Eyes Reading

--- Level 5 - Cinque ✋/​Eyes Reading/README.md
+++ Level 5 - Cinque ✋/​Eyes Reading/README.md
@@ -27,10 +27,11 @@ Those seem to relate to the game Noita:
 - https://scienceblogs.de/klausis-krypto-kolumne/2022/12/21/neues-zu-den-noita-kryptogrammen/
 - https://github.com/ngraham20/NoitaCryptographyResearch/tree/master
 - https://docs.google.com/document/d/1s6gxrc1iLJ78iFfqC2d4qpB9_r_c5U5KwoHVYFFrjy0/edit
+- https://docs.google.com/document/d/12sCi3OrTuy4PPcu3zUykue7suHvAPyK-uFKcm8Rp4Go/edit
 
-After comparing all patterns with the game Noita, we can conclud that only west-5 is added and the rest already existed in the game. So we focus to west-5 only:
+After comparing all patterns with the game Noita, we can conclude that only west-5 is added and the rest already existed in the game. So we focus to west-5 only:
 
-The game converts these strings of numbers into eye glyphs using the following table:
+The game converts these strings of eyes into number glyphs using the following table:
 
 ![translation.png](images/translation.png)
 
@@ -65,8 +66,24 @@ l = left
 c = center
 
 ### Calculating the cipher
+For implementing the Trigram cipher plus the transformations base5 to int, for each int + 32 and then to ASCII, I have created another script myself:
+
+[solver.py](solver.py)
+
+The output was made visible to always be capable in crosschecking the result with the originl image:
+
+    {1: ['trc', 'cdt', 'dcd', 'rlt', 'dtr', 'rdc', 'dcd', 'cld', 'rcd', 'dct', 'rdc', 'rcr', 'rld', 'cdt', 'dcd', 'crr', 'ccc', 'rtr', 'dcl', 'rcd', 'ccc', 'dcr', 'rdc', 'dcd', 'rdc', 'rlt'], 2: ['rdl', 'rdd', 'ccc', 'rcr', 'cdt', 'ccc', 'dtr', 'rdl', 'rdc', 'rdd', 'ccc', 'rdc', 'dcd', 'rdd', 'ccc', 'dtc', 'rdc', 'dtr', 'clt', 'rdl', 'ccc', 'cld', 'rlr', 'rdl', 'ccc', 'rdl'], 3: ['rtr', 'rdl', 'ccl', 'cct', 'ccc', 'tdc', 'rdl', 'dtr', 'rdl', 'ccc', 'rld', 'clt', 'ccc', 'rtr', 'cdt', 'rcd', 'dtr', 'ccc', 'rlc', 'dct', 'rdc', 'rlt', 'tct', 'ccc', 'rlr', 'rdl'], 4: ['cdd', 'cdt', 'cdd', 'clc', 'rtl', 'rdl', 'rtr', 'rdl', 'clt', 'rrd', 'ccl', 'cld', 'rld', 'dct', 'dct', 'rrd', 'dcc', 'rdl', 'rdl', 'dtc', 'rrd', 'rdc', 'rrd', 'clt', 'rdl', 'rdr'], 5: ['dtr', 'rdl', 'rcr', 'rrt', 'crl', 'ccc', 'rcc', 'rdl', 'dtc', 'dct', 'rdc', 'rdr', 'rdl', 'ccc', 'rdt', 'dtr', 'rdc', 'rdr', 'dcc', 'rdl', 'cld', 'ccl', 'ccc', 'rdt', 'rdl', 'rlc'], 6: ['cdt', 'dtr', 'rdl', 'ccc', 'rdr', 'rlr', 'rdl', 'rdr', 'dcc', 'rld', 'dcd', 'rlt', 'ccc', 'rld', 'dcd', 'cct']}
 
 
+    {1: ['120', '031', '303', '241', '312', '230', '303', '043', '203', '301', '230', '202', '243', '031', '303', '022', '000', '212', '304', '203', '000', '302', '230', '303', '230', '241'], 2: ['234', '233', '000', '202', '031', '000', '312', '234', '230', '233', '000', '230', '303', '233', '000', '310', '230', '312', '041', '234', '000', '043', '242', '234', '000', '234'], 3: ['212', '234', '004', '001', '000', '130', '234', '312', '234', '000', '243', '041', '000', '212', '031', '203', '312', '000', '240', '301', '230', '241', '101', '000', '242', '234'], 4: ['033', '031', '033', '040', '214', '234', '212', '234', '041', '223', '004', '043', '243', '301', '301', '223', '300', '234', '234', '310', '223', '230', '223', '041', '234', '232'], 5: ['312', '234', '202', '221', '024', '000', '200', '234', '310', '301', '230', '232', '234', '000', '231', '312', '230', '232', '300', '234', '043', '004', '000', '231', '234', '240'], 6: ['031', '312', '234', '000', '232', '242', '234', '232', '300', '243', '303', '241', '000', '243', '303', '001']}
+
+
+    [35, 16, 78, 71, 82, 65, 78, 23, 53, 76, 65, 52, 73, 16, 78, 12, 0, 57, 79, 53, 0, 77, 65, 78, 65, 71, 69, 68, 0, 52, 16, 0, 82, 69, 65, 68, 0, 65, 78, 68, 0, 80, 65, 82, 21, 69, 0, 23, 72, 69, 0, 69, 57, 69, 4, 1, 0, 40, 69, 82, 69, 0, 73, 21, 0, 57, 16, 53, 82, 0, 70, 76, 65, 71, 26, 0, 72, 69, 18, 16, 18, 20, 59, 69, 57, 69, 21, 63, 4, 23, 73, 76, 76, 63, 75, 69, 69, 80, 63, 65, 63, 21, 69, 67, 82, 69, 52, 61, 14, 0, 50, 69, 80, 76, 65, 67, 69, 0, 66, 82, 65, 67, 75, 69, 23, 4, 0, 66, 69, 70, 16, 82, 69, 0, 67, 72, 69, 67, 75, 73, 78, 71, 0, 73, 78, 1]
+
+    C0ngran7UlaTi0n, YoU managed T0 read and par5e 7he eYe$! Here i5 Y0Ur flag: he2024[eYe5_$7ill_keep_a_5ecreT]. Replace bracke7$ bef0re checking in!
+
+
+Credits for assistance: radio, Gempana
 
 ## The flag
-    he2024{}
+    he2024{eYe5_$7ill_keep_a_5ecreT}
--- Level 5 - Cinque ✋/​Eyes Reading/solver.py
+++ Level 5 - Cinque ✋/​Eyes Reading/solver.py
@@ -1,30 +1,80 @@
+def base5_decode_and_remove_zeros(numbers):
+    decoded_values = []
+    for num in numbers:
+        # Remove leading zeros from the number before decoding
+        num_without_zeros = str(num).lstrip('0')
+        if num_without_zeros == '':
+            num_without_zeros = '0'
+        # Convert the modified number to its base5 decoded value
+        decoded_value = int(num_without_zeros, 5)
+        decoded_values.append(decoded_value)
+    return decoded_values
+
+
 def solve_cipher():
-    cipher_as_text = {1: ['t', 'r', 't', 'd', 'c', 't', 'd', 't', 'c', 'd', 'c', 'd', 'r', 'c', 't', 'r', 'd', 'r', 'r', 'l', 't', 'd', 'c', 'r', 'c', 'c', 'r', 'd', 'c', 'd', 'c', 'c', 'r', 'r', 'd', 'd', 'r', 'd', 't'], 2: ['c', 'd', 'c', 'd', 'l', 'r', 'r', 'd', 'r', 'd', 'l', 'c', 'd', 'c', 'd', 'c', 'c', 'r', 'd', 'd', 'c', 'd', 'r', 'c', 'c', 't', 'r', 'l', 'c', 'r', 'c', 'c', 'd', 'c', 'c', 'd', 'c', 'l', 'r'], 3: ['r', 'd', 'd', 'c', 'c', 'r', 'c', 'd', 'c', 'd', 't', 'l', 'r', 'd', 'd', 'c', 'c', 'c', 'd', 'c', 'd', 'c', 'c', 'c', 'r', 'd', 'r', 'c', 'l', 'l', 'c', 'c', 'd', 'r', 'l', 'l', 'c', 'c', 'l'], 4: ['l', 'd', 'r', 'c', 'c', 'r', 't', 'c', 'c', 'r', 'd', 'r', 'c', 'd', 'r', 'c', 'd', 'r', 'd', 'd', 'r', 'c', 't', 'd', 'c', 't', 'd', 't', 'd', 'r', 'c', 'l', 'c', 'r', 'd', 'r', 'c', 'd', 'r'], 5: ['r', 't', 'l', 'c', 'c', 't', 'c', 'c', 'c', 'r', 'd', 'r', 'r', 'd', 'c', 'r', 'l', 't', 'c', 'c', 'r', 'c', 'd', 'd', 'd', 't', 'c', 'r', 'l', 't', 'r', 'd', 't', 't', 'c', 'c', 'r', 'l', 'l'], 6: ['r', 'd', 'r', 'l', 'c', 'c', 'c', 'd', 't', 'l', 't', 'd', 'l', 'c', 'c', 'd', 'l', 'c', 'c', 't', 'r', 't', 'c', 'r', 'r', 'c', 'c', 'c', 'c', 'd', 'c', 'l', 'r', 't', 'c', 'c', 'r', 'd', 'r'], 7: ['c', 'd', 't', 'c', 'd', 'c', 'r', 't', 'l', 'r', 't', 'l', 'c', 'l', 'd', 'c', 'c', 'd', 'r', 'l', 't', 'd', 'c', 'd', 'd', 'c', 'l', 'r', 'd', 'c', 'r', 'r', 'c', 'r', 'r', 't', 'r', 'd', 'r'], 8: ['d', 'd', 'c', 'd', 'l', 'c', 'l', 'd', 'r', 'r', 'd', 'r', 't', 'r', 'r', 'l', 'l', 'c', 'd', 'c', 'd', 't', 'r', 'r', 'c', 'd', 'r', 'l', 't', 'd', 'd', 'd', 'r', 'd', 'l', 'c', 'l', 'd', 'r'], 9: ['d', 't', 'l', 'r', 'c', 't', 'c', 'r', 'c', 'r', 'c', 'l', 'd', 't', 't', 'r', 'd', 'r', 'r', 'd', 'c', 'r', 'd', 'r', 'r', 'd', 'r', 'd', 'c', 'l', 'c', 'l', 'l', 'c', 'c', 't', 'r', 'd', 'c'], 10: ['r', 'd', 'r', 'r', 'r', 'r', 'l', 'c', 'c', 'c', 'd', 'r', 'c', 'c', 'd', 'c', 'd', 'r', 'l', 'c', 'c', 't', 't', 'd', 'c', 'd', 'r', 'c', 'd', 'r', 'd', 'c', 'c', 'c', 'd', 'r', 'l', 'l', 'r'], 11: ['c', 'd', 'r', 'r', 'd', 'c', 'r', 'd', 'r', 'r', 'd', 'r', 'd', 'c', 'd', 'd', 'c', 't', 'c', 'c', 'd', 'd', 'c', 't'], 12: ['t', 't', 'd', 'l', 'c', 'c', 'r', 'l', 'r', 'l', 'd', 'r', 'c', 'l', 'r', 'd', 'l', 'r', 'c', 'l', 'r', 'd', 'c', 'c']}
+    cipher_as_text = {
+        1: ['t', 'r', 't', 'd', 'c', 't', 'd', 't', 'c', 'd', 'c', 'd', 'r', 'c', 't', 'r', 'd', 'r', 'r', 'l', 't', 'd', 'c', 'r', 'c', 'c', 'r', 'd', 'c', 'd', 'c', 'c', 'r', 'r', 'd', 'd', 'r', 'd', 't'], 
+        2: ['c', 'd', 'c', 'd', 'l', 'r', 'r', 'd', 'r', 'd', 'l', 'c', 'd', 'c', 'd', 'c', 'c', 'r', 'd', 'd', 'c', 'd', 'r', 'c', 'c', 't', 'r', 'l', 'c', 'r', 'c', 'c', 'd', 'c', 'c', 'd', 'c', 'l', 'r'], 
+        3: ['r', 'd', 'd', 'c', 'c', 'r', 'c', 'd', 'c', 'd', 't', 'l', 'r', 'd', 'd', 'c', 'c', 'c', 'd', 'c', 'd', 'c', 'c', 'c', 'r', 'd', 'r', 'c', 'l', 'l', 'c', 'c', 'd', 'r', 'l', 'l', 'c', 'c', 'l'], 
+        4: ['l', 'd', 'r', 'c', 'c', 'r', 't', 'c', 'c', 'r', 'd', 'r', 'c', 'd', 'r', 'c', 'd', 'r', 'd', 'd', 'r', 'c', 't', 'd', 'c', 't', 'd', 't', 'd', 'r', 'c', 'l', 'c', 'r', 'd', 'r', 'c', 'd', 'r'], 
+        5: ['r', 't', 'l', 'c', 'c', 't', 'c', 'c', 'c', 'r', 'd', 'r', 'r', 'd', 'c', 'r', 'l', 't', 'c', 'c', 'r', 'c', 'd', 'd', 'd', 't', 'c', 'r', 'l', 't', 'r', 'd', 't', 't', 'c', 'c', 'r', 'l', 'l'], 
+        6: ['r', 'd', 'r', 'l', 'c', 'c', 'c', 'd', 't', 'l', 't', 'd', 'l', 'c', 'c', 'd', 'l', 'c', 'c', 't', 'r', 't', 'c', 'r', 'r', 'c', 'c', 'c', 'c', 'd', 'c', 'l', 'r', 't', 'c', 'c', 'r', 'd', 'r'], 
+        7: ['c', 'd', 't', 'c', 'd', 'c', 'r', 't', 'l', 'r', 't', 'l', 'c', 'l', 'd', 'c', 'c', 'd', 'r', 'l', 't', 'd', 'c', 'd', 'd', 'c', 'l', 'r', 'd', 'c', 'r', 'r', 'c', 'r', 'r', 't', 'r', 'd', 'r'], 
+        8: ['d', 'd', 'c', 'd', 'l', 'c', 'l', 'd', 'r', 'r', 'd', 'r', 't', 'r', 'r', 'l', 'l', 'c', 'd', 'c', 'd', 't', 'r', 'r', 'c', 'd', 'r', 'l', 't', 'd', 'd', 'd', 'r', 'd', 'l', 'c', 'l', 'd', 'r'], 
+        9: ['d', 't', 'l', 'r', 'c', 't', 'c', 'r', 'c', 'r', 'c', 'l', 'd', 't', 't', 'r', 'd', 'r', 'r', 'd', 'c', 'r', 'd', 'r', 'r', 'd', 'r', 'd', 'c', 'l', 'c', 'l', 'l', 'c', 'c', 't', 'r', 'd', 'c'], 
+        10: ['r', 'd', 'r', 'r', 'r', 'r', 'l', 'c', 'c', 'c', 'd', 'r', 'c', 'c', 'd', 'c', 'd', 'r', 'l', 'c', 'c', 't', 't', 'd', 'c', 'd', 'r', 'c', 'd', 'r', 'd', 'c', 'c', 'c', 'd', 'r', 'l', 'l', 'r'], 
+        11: ['c', 'd', 'r', 'r', 'd', 'c', 'r', 'd', 'r', 'r', 'd', 'r', 'd', 'c', 'd', 'd', 'c', 't', 'c', 'c', 'd', 'd', 'c', 't'], 
+        12: ['t', 't', 'd', 'l', 'c', 'c', 'r', 'l', 'r', 'l', 'd', 'r', 'c', 'l', 'r', 'd', 'l', 'r', 'c', 'l', 'r', 'd', 'c', 'c']}
     eye_direction_based_on_trigrams = {1:[],2:[],3:[],4:[],5:[],6:[]}
     number_based_on_trigrams = {1:[],2:[],3:[],4:[],5:[],6:[]}
-    for i in range(1,7):
+    result_index = 1
+    for i in range(1, 13, 2):
         line_length = len(cipher_as_text[i])
         pointer_1 = 0
         pointer_2 = 0
         iteration = 1
         while pointer_1 < line_length:
             if iteration == 1:
-                eye_direction_based_on_trigrams[i].append(cipher_as_text[i][pointer_1] + cipher_as_text[i][pointer_1+1] + cipher_as_text[i+1][pointer_2])
+                eye_direction_based_on_trigrams[result_index].append(cipher_as_text[i][pointer_1] + cipher_as_text[i][pointer_1+1] + cipher_as_text[i+1][pointer_2])
                 pointer_1 = pointer_1 + 2
                 pointer_2 = pointer_2 + 1
                 iteration = 2
             elif iteration == 2:
-                eye_direction_based_on_trigrams[i].append(cipher_as_text[i+1][pointer_2+1] + cipher_as_text[i+1][pointer_2] + cipher_as_text[i][pointer_1])
+                eye_direction_based_on_trigrams[result_index].append(cipher_as_text[i+1][pointer_2+1] + cipher_as_text[i+1][pointer_2] + cipher_as_text[i][pointer_1])
                 pointer_1 = pointer_1 + 1
                 pointer_2 = pointer_2 + 2
                 iteration = 1
 
-        for entry in eye_direction_based_on_trigrams[i]:
-            number_based_on_trigrams[i].append(str(entry).replace('c','0').replace('t','1').replace('r','2').replace('d','3').replace('l','4'))
+        # print(eye_direction_based_on_trigrams)
+        for entry in eye_direction_based_on_trigrams[result_index]:
+            number_based_on_trigrams[result_index].append(str(entry).replace('c','0').replace('t','1').replace('r','2').replace('d','3').replace('l','4'))
+
+        
+        result_index = result_index + 1
 
 
+    # print(eye_direction_based_on_trigrams)
+    # print(number_based_on_trigrams)
+
+    trigram_lines_base5 = []
+    for k,v in enumerate(number_based_on_trigrams):
+        decoded_values = base5_decode_and_remove_zeros(number_based_on_trigrams[v])
+        trigram_lines_base5.extend(decoded_values)
+    
     print(eye_direction_based_on_trigrams)
+    print("\n")
     print(number_based_on_trigrams)
+    print("\n")
+    print(trigram_lines_base5)
+
+    flag = ""
+    for num in trigram_lines_base5:
+        flag = flag + (chr(num + 32))
+
+    print("\n{0}".format(flag))
+
+
+
 
 
 def main():
